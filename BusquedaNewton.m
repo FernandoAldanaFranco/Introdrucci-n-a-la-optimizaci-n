d02f08c6719ae56a21a898574dfd9f5cdbf14058
Fernando Aldana Franco <96087163+FernandoAldanaFranco@users.noreply.github.com>
2025-08-24 21:18:24 -0600

    Método de Newton

---
+++ BusquedaNewton.m
@@ -0,0 +1,49 @@
+%Algoritmo de optimización búsqueda de Newton.
+%Fernando Aldana
+%Agosto 2025
+%Ejemplo basado en la función (x-2)^2 en el intervalo [a,b]
+%%
+%Limpia y cierra
+clc;
+clear all;
+close all;
+% Variables
+punto=5; %Valor inicial
+a=0;
+b=5; %Rango
+iteraciones=7; %Número de iteraciones
+e=0.1;
+%%
+%Declara las funciones como simbólicas
+syms x; %Variable simbólica
+f=(x-2)^2; %Declara la función
+fx=diff(f, x); %Primera derivada
+fxx=diff(fx, x); %Segunda derivada
+%%
+%Proceso iterativo
+for i=1: iteraciones
+    
+    r=subs(f,x,punto); %Evalua función, 
+    s=subs(fx,x,punto); %primera derivada y
+    t=subs(fxx,x,punto); %segunda derivada
+    temporal=punto;
+    punto=punto-(s/t)%Calcula nuevo punto
+    %Verifica si el punto aproximado está dentro de los límites de búsqueda
+    if punto<a
+        punto=a;
+    end
+    if punto>b
+        punto=b;
+    end
+    if s==0 %Punto crítico
+        if t>0 %mínimo local
+            punto=temporal;%No actualiza le punto calculado
+        else
+        end
+    else
+        
+    end
+end
+%Evalua la función en los últimos dos puntos para obtener una respuesta
+punto
+subs(f,x,punto)
